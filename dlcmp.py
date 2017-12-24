676c9c78530be2be22b649a370be9ba39b9ad096
InkPrism <contact@dotsilver.net>
2017-12-24 15:52:18 +0100

    "private"

--- dlcmp.py
+++ dlcmp.py
@@ -8,7 +8,7 @@ import urllib.parse
 import posixpath
 
 
-def getheader(resp, head):
+def _getheader(resp, head):
     # Try to retrieve the specified header
     try:
         return resp.info()[head]
@@ -16,13 +16,13 @@ def getheader(resp, head):
         return "-"
 
 
-def report(content, silent, end=os.linesep):
+def _report(content, silent, end=os.linesep):
     if not silent:
         print(content, end=end)
 
 
-def log_failed(content, log, silent):
-    report(content, silent)
+def _log_failed(content, log, silent):
+    _report(content, silent)
     if log is not None:
         try:
             with open(log, 'a') as f:
@@ -32,7 +32,7 @@ def log_failed(content, log, silent):
             pass
 
 
-def req(url, ua_head):
+def _req(url, ua_head):
     req = urllib.request.Request(url)
     req.add_header('User-Agent', ua_head)
     return req
@@ -40,8 +40,8 @@ def req(url, ua_head):
 
 def dl(manifest, log=None, user_agent="-", verbose=False, cache=None, silent=False):
     import json
-    report('\n[*] ' + str(manifest), silent)
-    report('rtfm...', silent)
+    _report('\n[*] ' + str(manifest), silent)
+    _report('rtfm...', silent)
     # Concrete path
     manifestpath = Path(manifest)
     # Get the parent
@@ -63,9 +63,9 @@ def dl(manifest, log=None, user_agent="-", verbose=False, cache=None, silent=Fal
     # Fancy counter
     currF = 1
     allF = len(manifestjson['files'])
-    report(str(allF) + ' mods found.', silent)
+    _report(str(allF) + ' mods found.', silent)
 
-    report('Downloading files...', silent)
+    _report('Downloading files...', silent)
 
     # Check, if a cache path was given...
     cachedl = False
@@ -74,7 +74,7 @@ def dl(manifest, log=None, user_agent="-", verbose=False, cache=None, silent=Fal
             cachepath = Path(cache)
             cachedl = True
         else:
-            report(str(cache) + " is no directory or cannot be accessed as such. Continuing without cache.", silent)
+            _report(str(cache) + " is no directory or cannot be accessed as such. Continuing without cache.", silent)
 
     # The magic...
     for dependency in manifestjson['files']:
@@ -93,9 +93,9 @@ def dl(manifest, log=None, user_agent="-", verbose=False, cache=None, silent=Fal
         try:
             # Open file URL
             filepath = projecturl + '/files/' + str(dependency['fileID']) + '/download'
-            projectresp = urllib.request.urlopen(req(filepath, user_agent))
+            projectresp = urllib.request.urlopen(_req(filepath, user_agent))
         except urllib.error.HTTPError as e:
-            log_failed(str(e.code) + ' - ' + filepath, log, silent)
+            _log_failed(str(e.code) + ' - ' + filepath, log, silent)
             currF += 1
             continue
 #        # Get fileName from header
@@ -106,13 +106,13 @@ def dl(manifest, log=None, user_agent="-", verbose=False, cache=None, silent=Fal
         filename = posixpath.basename(path)
         filename = filename.replace('%20', ' ')
         # Retrieve and write file
-        report('[' + str(currF) + '/' + str(allF) + '] ' + str(filename), silent, end='')
+        _report('[' + str(currF) + '/' + str(allF) + '] ' + str(filename), silent, end='')
         # Get file size from header if verbose is true
         if verbose:
-            report(" - %s bytes" % getheader(projectresp, "Content-Length"), silent, end="")
+            _report(" - %s bytes" % _getheader(projectresp, "Content-Length"), silent, end="")
         # If file is already exists, skip
         if os.path.isfile(str(minecraftpath / "mods" / filename)):
-            report(' - SKIPPED', silent)
+            _report(' - SKIPPED', silent)
         else:
             with open(str(minecraftpath / "mods" / filename), "wb") as mod:
                 mod.write(projectresp.read())
@@ -122,37 +122,37 @@ def dl(manifest, log=None, user_agent="-", verbose=False, cache=None, silent=Fal
                 if os.path.exists(targetcachepath):
                     targetcachepath.mkdir(parents=True)
                 shutil.copyfile(str(minecraftpath / "mods" / filename), str(cachepath / str(dependency['projectID']) / str(dependency['fileID']) / filename))
-            report(" - Done", silent)
+            _report(" - Done", silent)
         currF += 1
-    report('Catched \'em all!', silent)
+    _report('Catched \'em all!', silent)
     return
 
 
 def get_modpack(url, log=None, user_agent="-", verbose=False, cache=None, silent=False):
     import zipfile
-    report('\n[*] ' + str(url), silent)
-    report('Starting download...', silent)
+    _report('\n[*] ' + str(url), silent)
+    _report('Starting download...', silent)
     try:
         to_file = '/download'
         if url.endswith(to_file):
             to_file = ''
         # Retrieving modpack
-        dl_mp = urllib.request.urlopen(req(url + to_file, user_agent))
+        dl_mp = urllib.request.urlopen(_req(url + to_file, user_agent))
         resp = dl_mp.read()
         # Get the name for the file by getting the redirect from '/download' to 'something/something.extenshion'
         dl_mp = dl_mp.geturl()
         dl_mp = dl_mp.replace("%20", " ")
         filename = posixpath.basename(dl_mp)
-        report('Downloading ' + filename, silent)
+        _report('Downloading ' + filename, silent)
     except urllib.error.HTTPError as e:
-        log_failed(str(e.code) + ' - ' + url, log, silent)
+        _log_failed(str(e.code) + ' - ' + url, log, silent)
         return
     try:
         with open(filename, "wb") as f:
             f.write(resp)
     except (OSError, IOError) as e:
-        report(e, silent)
-        report('Unable to write data from ' + str(url) + ' to ' + str(filename), silent)
+        _report(e, silent)
+        _report('Unable to write data from ' + str(url) + ' to ' + str(filename), silent)
         return
     # Create new dir for extracted files
     dirname = filename
@@ -161,8 +161,8 @@ def get_modpack(url, log=None, user_agent="-", verbose=False, cache=None, silent
         dirname = dirname[:-4]
     # If Dir already exist
     if os.path.isdir(str(Path(dirname))):
-        report('Dir ' + str(dirname) + ' already exists.', silent)
-        report('To not go at risk of destroying data, the procedure will be stopped.', silent)
+        _report('Dir ' + str(dirname) + ' already exists.', silent)
+        _report('To not go at risk of destroying data, the procedure will be stopped.', silent)
         return
     # Create the Dir
     os.makedirs(str(Path(dirname)))
@@ -190,12 +190,12 @@ def main():
     arg("--silent", dest='silent', help="no output to cli", action='store_true', default=False)
     args, unknown = parser.parse_known_args()
     if args.verbose:
-        report('Log: ' + str(args.log), args.silent)
-        report('User-Agent: ' + str(args.useragent), args.silent)
+        _report('Log: ' + str(args.log), args.silent)
+        _report('User-Agent: ' + str(args.useragent), args.silent)
     if args.dest is None:
         if not args.silent:
             parser.print_usage()
-        report('No positional argument found. Aborting.', args.silent)
+        _report('No positional argument found. Aborting.', args.silent)
         return
     # Test, if it is a url (with bad regex) and not specified as path (or if it is specified as url)
     match = re.match(r'^(?:(?:http|ftp)s?://).*$', args.dest, re.IGNORECASE)
@@ -204,7 +204,7 @@ def main():
     # Specified as path?
     else:
         if not os.path.isfile(args.dest):
-            report('No manifest found at %s' % args.dest, args.silent)
+            _report('No manifest found at %s' % args.dest, args.silent)
             return
         dl(args.dest, log=args.log, user_agent=args.useragent, verbose=args.verbose, cache=args.cache, silent=args.silent)
 
